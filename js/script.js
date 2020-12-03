a6bf0fb375fbcbe9a50099280c80504bc9cdfafd
MarcoB <mrc.bsllt@gmail.com>
2020-12-03 13:49:43 +0100

    primo commit test

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// milestone 1:
+// definire un array di oggetti; ogni oggetto
+// rappresenta un'icona, che è caratterizzata da:
+// nome, prefisso, tipo e famiglia.
+// Utilizzando la funzione forEach e il template
+// literal, visualizzare in pagina tutte le icone con il
+// proprio nome.
+
+// milestone 2:
+// definire un array di colori e associare ad ogni
+// tipo di icona un colore.
+// Visualizzare le icone di colore diverso in base al
+// tipo.
+
+// milestone 3:
+// aggiungere una select per filtrare le icone in
+// base al tipo.
+// Popolare le options della select dinamicamente
+// e, ogni volta che cambia il valore selezionato,
+// visualizzare le icone corrispondenti.
+
+$(document).ready(
+  function() {
+  }
+);
